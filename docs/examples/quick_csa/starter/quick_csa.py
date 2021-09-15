77d0a86aa144b095f9c24066e49ed3579ed534b0
David Kastner <kastner@mit.edu>
2021-09-14 22:43:30 -0400

    Update quick_csa.py

--- docs/examples/quick_csa/starter/quick_csa.py
+++ docs/examples/quick_csa/starter/quick_csa.py
@@ -41,10 +41,10 @@ def clean_dir():
     for dir in file_system:
         if os.path.isdir(dir):
             file_system_exists = True
-            file_mover(file_system_exists, pdb_name)
         else:
             os.mkdir(dir)
-
+    file_mover(file_system_exists, pdb_name)
+    
     return pdb_name
 
 
@@ -63,6 +63,8 @@ Returns
 def file_mover(file_system_exists, pdb_name):
     required_files = [pdb_name, 'apo_list',
                       'holo_list', 'apo_charge.xls', 'holo_charge.xls']
+    print(file_system_exists)
+    print('hi')
     for file in required_files:
         file_already_moved = False
         if file_system_exists:
@@ -385,4 +387,3 @@ def quick_csa():
 # Execute the Quick CSA when run as a script but not if used as a pyQM/MM module
 if __name__ == "__main__":
     quick_csa()
-
