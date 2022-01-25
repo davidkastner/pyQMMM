0ec289357db05ecadc9865f2708dfcabcddc8ba0
David Kastner <kastner@mit.edu>
2022-01-25 17:34:31 -0500

    Update xyz_combine.py

--- pyqmmm/xyz_combine.py
+++ pyqmmm/xyz_combine.py
@@ -24,31 +24,58 @@ Get the request frames for each file from the user.
 Returns
 -------
 frames : list
-    List of lists containing the frames the user wants from each file
+    List of lists containing the frames the user wants from each file in order
 '''
+
+
 def requested_frames():
-    request = ''
-    while 
-    # What frames would you like from the first .xyz file
-    request = input('Frames from first file? (e.g., 1_2): ')
+    request = 'start'
+    file_count = 0
+    frames_list = []
 
-    # Check the request and convert it to a list even if it is hyphenated
-    temp = [(lambda sub: range(sub[0], sub[-1] + 1))(list(map(int, ele.split('-')))) for ele in request.split('_')] 
-    frames = [b for a in temp for b in a] 
+    while request != '':
+        # What frames would you like from the first .xyz file
+        request = input('Frames from file #{}? (1_2): '.format(file_count))
 
-    return frames
+        if request != '':
+            # Check the request and convert it to a list even if it is hyphenated
+            temp = [(lambda sub: range(sub[0], sub[-1] + 1))
+                    (list(map(int, ele.split('-')))) for ele in request.split('_')]
+            frames = [b for a in temp for b in a]
+            frames_list.append(frames)
 
-# General function handler
+    return frames_list
+
+
+'''
+Loop through the coordinate files and store each frame as a list element
+Parameters
+-------
+atoms : list
+    list of atoms indices
+Get the user's reaction coordinate definition.
+Returns
+-------
+atoms : list
+    list of atoms indices
+'''
+
+
+def store_frames():
+
+    # General function handler
+
+
 def xyz_combine():
-    print('\n.------------------------.')
-    print('| WELCOME TO XYZ COMBINE |')
-    print('.------------------------.\n')
+    print('\N.------------------.')
+    print('| COMBINE XYZ FILES|')
+    print('.------------------.\n')
     print('Add both xyz trajectories to the current directory.')
     print('You can combine as many xyz files as you need.')
     print('Leave the prompt blank when you are done.\n')
 
     # Find out what frames the user wants combined
-    frames = requested_frames()
+    frames_list = requested_frames()
 
 
 if __name__ == "__main__":
