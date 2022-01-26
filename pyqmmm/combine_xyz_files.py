0f5a8925c8a6f0f42f7b98a840db1b451b758483
David Kastner <kastner@mit.edu>
2022-01-25 20:51:03 -0500

    Update combine_xyz_files.py

--- pyqmmm/combine_xyz_files.py
+++ pyqmmm/combine_xyz_files.py
@@ -93,6 +93,49 @@ def request_frames(xyz_filename_list):
     return file_request_dict
 
 
+'''
+Turns a xyz trajectory file into a list of lists where each element is a frame.
+Parameters
+----------
+xyz_filename : string
+    The file name of a trajectory
+
+Returns
+-------
+trajectory_list : list
+    List of lists containing the trajectory with each frame saved as an element
+'''
+
+
+def multiframe_xyz_to_list(xyz_filename):
+    # Variables that measure our progress in parsing the optim.xyz file
+    xyz_list = []  # List of lists containing all frames
+    frame_contents = ''
+    line_count = 0
+    frame_count = 0
+    first_line = True  # Marks if we've looked at the atom count yet
+
+    # Loop through optim.xyz and collect distances, energies and frame contents
+    with open(xyz_filename, 'r') as trajectory:
+        for line in trajectory:
+            # We determine the section length using the atom count in first line
+            if first_line == True:
+                section_length = int(line.strip()) + 2
+                first_line = False
+            # At the end of the section reset the frame-specific variables
+            if line_count == section_length:
+                line_count = 0
+                frame_contents = ''
+                frame_count += 1
+
+            frame_contents += line
+            line_count += 1
+
+        xyz_list.append(frame_contents)
+
+    return xyz_list
+
+
 def combine_xyz_files():
     # Welcome the user to the file and introduce basic functionality
     print('\n.-------------------.')
@@ -109,7 +152,7 @@ def combine_xyz_files():
     # Find out what frames the user wants combined
     file_request_dict = request_frames(xyz_filename_list)
 
-    # Save each frame of the trajectory to an list
+    #
 
 
 if __name__ == "__main__":
