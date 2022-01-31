708c6873bad9f3408f5980fa5a9dada5b5010a87
David Kastner <kastner@mit.edu>
2022-01-30 23:45:59 -0500

    Update extract_charges_spins.py

--- pyqmmm/extract_charges_spins.py
+++ pyqmmm/extract_charges_spins.py
@@ -10,7 +10,35 @@ DESCRIPTION
 
 '''
 
+################################ DEPENDENCIES ##################################
+import glob
+import matplotlib.pyplot as plt
+from scipy.spatial import distance
+import numpy as np
+import collect_reaction_coordinate
+
 ################################## FUNCTIONS ###################################
+'''
+Find all examples of a file type in the current directory
+Parameters
+----------
+file_pattern : str
+    The type of file the user would like to search the current directory for
+Returns
+-------
+xyz_filename_list : list
+    list of files found matching the user's pattern
+'''
+
+
+def get_files(file_pattern):
+    file_list = glob.glob(file)
+    sorted(file_list)
+    file_list = []
+    print('We found {} files for the patter {}'.format(file_list, file_pattern))
+    return file_list
+
+
 '''
 Get the user's atom set.
 Returns
@@ -41,12 +69,12 @@ atoms : list
 '''
 
 
-def get_spins(atoms):
+def get_spins(atoms, file, selection):
     # Sum the spins for the user selected atoms
     net_spins = []
     net_spin = 0
     scan_step_count = 0
-    with open('./scr/mullpop', 'r') as scan_spin_file:
+    with open(file, 'r') as scan_spin_file:
         for line in scan_spin_file:
             line_list = line.split()
             if line_list[0] in atoms:
@@ -57,9 +85,11 @@ def get_spins(atoms):
                 net_spins.append('{} {}\n'.format(scan_step_count, net_spin))
                 net_spin = 0
 
-    with open('./spin.dat', 'w') as select_spin_file:
-        for pair in net_spins:
-            select_spin_file.write(pair)
+    reverse = input('Press any key to reverse data for {}: '.format(file))
+    if reverse:
+        net_spins.reverse()
+
+    return net_spins
 
 
 '''
@@ -71,12 +101,12 @@ atoms : list
 '''
 
 
-def get_charges(atoms):
+def get_charges(atoms, file, selection):
     # Sum the charges for the user selected atoms
     net_charges = []
     net_charge = 0
     scan_step_count = 0
-    with open('./scr/charge_mull.xls', 'r') as scan_charge_file:
+    with open(file, 'r') as scan_charge_file:
         for line in scan_charge_file:
             line_list = line.split()
             if line_list[0] in atoms:
@@ -88,20 +118,50 @@ def get_charges(atoms):
                     scan_step_count, net_charge))
                 net_charge = 0
 
-    with open('./charge.dat', 'w') as select_charge_file:
-        for pair in net_charges:
-            select_charge_file.write(pair)
+    reverse = input('Press any key to reverse data for {}: '.format(file))
+    if reverse:
+        net_charges.reverse()
+
+    return net_charges
+
+
+'''
+Writes out the data for either the charge or spin
+Parameters
+----------
+file : str
+    the name of the file where we are going to write out the data
+net_data : list
+    list of data extracted from either the spin or charge files
+'''
+
+
+def write_data(file, net_data):
+    with open(file, 'w') as select_file:
+        for pair in net_data:
+            select_file.write(pair)
 
 
 def extract_charges_spins():
     print('\n.-----------------------.')
     print('| EXTRACT CHARGES SPINS |')
     print('.-----------------------.\n')
-    print('First run pes_data_organizer.py')
-    print('Extracts summed charge and spin for user specified atoms\n')
+    print('First run pes_data_organizer.py for each job.')
+    print('Move the scan_charge and scan_spin to the same directory.')
+    print('Give them unique names.')
+    print('Extract summed charge and spin for user specified atoms\n')
 
+    # Check how many charge and spin files
+    charge_files = get_files('*.charge')
+    spin_files = get_files('*.spin')
+
+    # What atoms does the user want to perform charge-spin analysis for?
     atoms = user_input()
-    get_charges(atoms)
+
+    for file in charge_files:
+        selection = input('What frames would you like for {}'.format(file))
+        net_charge_data = get_charges(atoms, file)
+
     get_spins(atoms)
 
 
