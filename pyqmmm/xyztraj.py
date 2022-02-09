21616b838d6a74c2b89d7bcf8f313db7d8c4468d
David Kastner <kastner@mit.edu>
2022-02-08 22:33:56 -0500

    Update xyztraj.py

--- pyqmmm/xyztraj.py
+++ pyqmmm/xyztraj.py
@@ -1,24 +1,25 @@
-# @file XYZTraj.py
-#  Defines xyztraj class and manipulation methods.
+# @file MolTraj.py
+#  Defines MolTraj class and trajectory manipulation methods.
 #
-#  Written by David Kastner
+#  Written by David W. Kastner
 #
 #  Department of Chemical Engineering, MIT
 
-class XYZTraj:
+class MolTraj:
   '''
-  Holds information about a molecular trajectory, used to do maniplualtions.
-  Reads informaiton from structural xyz file.
+  Stores a molecular trajectory and facilitates manipulations.
+  Reads information from an multiframe xyz file.
   
-  Example instantiation of a molecular scan from an XYZ trajectory:
+  Example instantiation of a molecular scan from an molecular trajectory:
   
-  >>> mol_scan = traj3D()
+  >>> mol_scan = MolTraj()
   >>> mol_scan.get_xyz('tc_scan.xyz')
   '''
   
-  def __init__(self, file_name=''):
+  def __init__(self, name=''):
     # The number of frames
     self.frames = []
-  
-  
+    
+  def get_traj(self, filename):
+    return
     
