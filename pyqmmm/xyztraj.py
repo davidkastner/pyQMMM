ff8abd1be2c9ee9592e5fa0f63c973963ce8ec01
David Kastner <kastner@mit.edu>
2022-02-08 22:16:24 -0500

    Update xyztraj.py

--- pyqmmm/xyztraj.py
+++ pyqmmm/xyztraj.py
@@ -1,1 +1,13 @@
-from .renumber_residues import renumber_residues
+class xyztraj3D:
+  '''
+  Holds information about a molecular trajectory, used to do maniplualtions.
+  Reads informaiton from structural xyz file.
+  
+  Example instantiation of a molecular scan from an XYZ trajectory:
+  
+  >>> mol_scan = traj3D()
+  >>> mol_scan.get_xyz('tc_scan.xyz')
+  '''
+  
+  def __init__(self, pdbCode=''):
+    
