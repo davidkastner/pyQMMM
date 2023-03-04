b88928c0bf555f22b0eeef7608adf43cdf8f2030
David Kastner <kastner@mit.edu>
2023-03-03 23:29:20 -0500

    Update modeller example and automodeller

--- pyqmmm/md/automodeller.py
+++ pyqmmm/md/automodeller.py
@@ -3,27 +3,43 @@
 from modeller import *
 from modeller.automodel import *
 
+def modeller_step1(pdb_code):
+    """
+    Writes the alignment file.
 
-def modeller_step1():
+    Parameters
+    ----------
+    pdb_code : str
+        The PDB code of the protein to be modelled.
+
+    Returns
+    -------
+
     """
-    Takes a PDB accession when prompted and generates a sequence file
-    """
-    code = input("Enter a PDB accession: ")
     e = Environ()
-    m = Model(e, file=code)
+    m = Model(e, file=pdb_code)
     aln = Alignment(e)
-    aln.append_model(m, align_codes=code)
-    aln.write(file=code + ".seq")
-    print("A sequence file for {} was generated.".format(code))
+    aln.append_model(m, align_codes=pdb_code)
+    aln.write(file=f"{pdb_code}.seq")
 
+    print(f"A sequence file for {pdb_code} was generated.")
 
-def modeller_step2():
+def modeller_step2(pdb_code):
+    """
+    Models in missing residues.
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+
+    """
     log.verbose()
     env = Environ()
 
     # directories for input atom files
     env.io.atom_files_directory = [".", "../atom_files"]
-    code = input("Enter a PDB accession: ")
 
     class MyModel(AutoModel):
         def select_atoms(self):
@@ -31,10 +47,17 @@ def modeller_step2():
                 self.residue_range("1:A", "10:A"), self.residue_range("169:A", "173:A")
             )
 
-    a = MyModel(
-        env, alnfile="{}.ali".format(code), knowns=code, sequence="{}_fill".format(code)
-    )
+    a = MyModel(env, alnfile=f"{pdb_code}.ali", knowns=pdb_code, sequence=f"{pdb_code}_fill")
+
     a.starting_model = 1
     a.ending_model = 1
 
     a.make()
+
+if __name__ == "__main__":
+    # Initialize variables that can be obtained from protein3D
+    pdb_code = "1OS7"
+    
+    # Modeller workflow
+    modeller_step1(pdb_code)
+    modeller_step2(pdb_code)
