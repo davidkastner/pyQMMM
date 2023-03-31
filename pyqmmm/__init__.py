06dd7b63d2f6feb70cc0d64a8354b7791a49c8a1
David Kastner <kastner@mit.edu>
2023-03-31 12:54:55 -0400

    Update framework

--- pyqmmm/__init__.py
+++ pyqmmm/__init__.py
@@ -1,11 +1,3 @@
-"""Python package for manipulating QM and MD trajectories"""
+"""Python package for working with multiscale simulation data."""
 
 # Add imports here
-# from pyqmmm import *
-
-# Handle versioneer
-from ._version import get_versions
-versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
-del get_versions, versions
